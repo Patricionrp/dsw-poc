a6e6233bb1c4948f565c669a1df31db4ffc9f6e8
Patricionrp <patricioriquelme330@gmail.com>
2026-08-10 14:52:04 -0300

    implemento ejemplo de uso de fixtures, support y commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,28 +1,23 @@
 describe('Compra de producto', () => {
 
-  it('Debe agregar un producto e iniciar el checkout', () => {
+    it('Debe agregar un producto e iniciar checkout', () => {
 
-    cy.visit('https://www.saucedemo.com/')
+        cy.visit('/')
 
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+        cy.fixture('usuario').then((datos) => {
 
-    
-    cy.contains('Products')
+            cy.login(datos.usuario, datos.password)
 
-    
-    cy.get('#add-to-cart-sauce-labs-backpack').click()
+        })
 
-    cy.get('.shopping_cart_link').click()
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
 
-    cy.get('.inventory_item_name')
-      .should('have.text', 'Sauce Labs Backpack')
+        cy.get('.shopping_cart_link').click()
 
-    cy.get('#checkout').click()
+        cy.get('#checkout').click()
 
-    cy.contains('Checkout: Your Information')
+        cy.contains('Checkout: Your Information')
 
-  })
+    })
 
 })
